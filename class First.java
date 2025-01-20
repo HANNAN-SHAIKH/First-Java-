b2b66d06b0dcdd8e9f69ec623d43e4f0481c490a
HANNAN-SHAIKH <47747985+HANNAN-SHAIKH@users.noreply.github.com>
2025-01-20 10:35:31 +0530

    Update class First.java

--- class First.java
+++ class First.java
@@ -1,4 +1,23 @@
 import java.io.*;
 class First
 {
-]
+  private int a;
+  private int b;
+  piublic static void sum()
+  {
+    system.out.println(a+b);
+  }
+  public static void sub()
+  {
+    system.out.println(a-b);
+  }
+  public static void main (string[] args)
+  {
+    First obj=new First();
+    obj.sum();
+    obj.sub();
+  
+  }
+  
+    
+  }
